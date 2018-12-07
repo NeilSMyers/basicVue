7ccb9eea985a8f1f6f8e139cf35b5d2f05a9bc9b
neilmyers <mmyersneil@gmail.com>
2018-12-07 16:16:41 -0700

    compound conditionals, links, event listeners

--- app.js
+++ app.js
@@ -3,14 +3,41 @@ new Vue({
   data: {
     name: "Neil",
     imgWidth: 150,
-    show: false
+    imgHeight: 150,
+    show: true,
+    smallImg: true,
+    medImg: false,
+    lgImg: false,
+    googleUrl: "https://www.google.com",
+    squaredResult: 0
   },
   methods: {
     greeting() {
       return `hello ${this.name}`
     },
-    toggle() {
-      this.show = !this.show
+    showImgOptions() {
+      if(this.smallImg) {
+        this.imgHeight = 250
+        this.imgWidth = 250
+        this.smallImg = false
+        this.medImg = true
+      } else if(this.medImg) {
+        this.imgHeight = 350
+        this.imgWidth = 350
+        this.medImg = false
+        this.lgImg = true
+      } else {
+        this.imgHeight = 150
+        this.imgWidth = 150
+        this.lgImg = false
+        this.smallImg = true
+      }
+    },
+    showGoogleUrl() {
+      console.log("showing details")
+    },
+    square(evt) {
+      return this.squaredResult = evt.target.value ** 2
     }
   }
 })
